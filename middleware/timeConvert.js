79dbf32a143905dcb8d6191ba3bb3838c7bf829e
Kevin Ruhnke <kevin.ruhnke@iconnewmedia.de>
2019-11-07 16:18:51 +0100

    timestart & timeend

--- middleware/timeConvert.js
+++ middleware/timeConvert.js
@@ -3,8 +3,8 @@ const moment = require("moment");
 module.exports = (req, res, next) => {
     const {date, timestart, timeend} = req.body;
     
-    req.body.timestart = moment(date + timestart, "DD.MM.YYYY HH:mm").format();
-    req.body.timeend = moment(date + timeend, "DD.MM.YYYY HH:mm").format();
+    req.body.timestart = timestart && moment(date + timestart, "DD.MM.YYYY HH:mm").format();
+    req.body.timeend = timeend && moment(date + timeend, "DD.MM.YYYY HH:mm").format();
     
     next();
 };
